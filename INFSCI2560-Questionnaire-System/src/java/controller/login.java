519c76f666194ca5e69e97600484b79eb6fe3120
qsis2140 <SHQ7@pitt.edu>
2016-12-09 16:33:12 -0500

    login and register add session with attributes"userName", "userID" relocate to "fali.jsp" when fail in both login and register 	 to "success.jsp" after a successful register 	 to "loginsuccess.jsp" after a successful login

---
+++ INFSCI2560-Questionnaire-System/src/java/controller/login.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import model.userBean;
+import dataAccessObject.userDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author qssheep
+ */
+//@WebServlet(name = "login", urlPatterns = {"/login"})
+public class login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    public void init(ServletConfig config) throws ServletException{
+        super.init(config);
+    }
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+       
+        HttpSession session = request.getSession();
+        userBean account = new userBean();
+        String username = request.getParameter("username");
+        String pwd = request.getParameter("pwd");
+        account.setPassword(pwd);
+        account.setUsername(username);
+        userDao ud = new userDao();
+        if(username != null) {
+            if(pwd != null) {
+                //System.out.println("success");
+                //session.setAttribute("account", account);
+                int userid = ud.retrieveUser(username, pwd);
+                if(userid!= -1){
+                    System.out.println("success");
+                    String login_suc = "loginsuccess.jsp";
+                    session.setAttribute("userName", username);
+                    session.setAttribute("userID", userid);
+                    response.sendRedirect(login_suc);
+                    return;
+                }
+            }
+        }
+        String login_fail = "fail.jsp";
+        response.sendRedirect(login_fail);
+        return;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ INFSCI2560-Questionnaire-System/web/success.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : success
+    Created on : 2016-11-21, 17:32:12
+    Author     : qssheep
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>succeeded!</h1>
+    </body>
+</html>
